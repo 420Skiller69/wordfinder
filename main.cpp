400f287dd3cb20953f10584b5f82fdc155bbd0f7
420Skiller69 <timmetz65@gmail.com>
2022-09-13 18:58:09 +0200

    tttt

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
 using namespace std;
 int main(int argc, char* args[])
 {
-	cout << endl << "ha wooooasdoooolorld" << endl  << endl << endl;
+	cout << endl << "ha wooooajamaasdoooolorld??????????????" << endl  << endl << endl;
 	return 0;
 }
