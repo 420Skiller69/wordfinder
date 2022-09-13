ffdba22c5a649b0f89fd0ed951d0aaa2249dfca5
420Skiller69 <timmetz65@gmail.com>
2022-09-13 18:47:04 +0200

    hahhahaa

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
 using namespace std;
 int main(int argc, char* args[])
 {
-	cout << endl << "ha wooolorld" << endl  << endl << endl;
+	cout << endl << "ha woooooolorld" << endl  << endl << endl;
 	return 0;
 }
