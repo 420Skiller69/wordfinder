de6de08c9f0d0b8a50b1930c4dcc507dd8d61117
420Skiller69 <timmetz65@gmail.com>
2022-09-16 01:01:21 +0200

    iteraros

--- main.cpp
+++ main.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
-
+#include <vector>
+#include <iterator>
 using namespace std;
+
+
+struct student
+{
+	char word[8];
+	double fitness;
+};
+double getFitness(struct student stud);
+
+
+vector<char> keyword = {'j', 'o', 'j', 'o', 'j', 'o', 'j', 'o'};
+
+
 int main(int argc, char* args[])
 {
-	int a;
-	cin >> a;
-	cout << endl << a << " ha wooolorld??????????????" << endl  << endl << endl;
+	// struct student s;
+	// s.word = keyword;
+	// cout << getFitness(s) << endl;
+	
+
+
+	vector<int> hallal = {1,2,2,3,3,6,6,6,6,6};
+	hallal[0] = 1473734876;
+	for(auto i: hallal) cout << i << " ";
+	
 	return 0;
+}
+
+double getFitness(struct student stud)
+{
+	double fitness = 0;
+	// for(int i = 0; i < keywordLEN; i++) fitness += stud.word[i] == keyword[i];
+	return fitness;
 }
